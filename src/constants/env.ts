909996be2576eac41289f2b1481734ec9ba488d7
Alan Wendorff <62715512+AlanWendorff@users.noreply.github.com>
2023-11-16 12:25:14 -0300

    feat: :necktie: Added business logic for change network button (#9)

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,3 +1,6 @@
 // WEB3
+export const TARGET_NETWORK_HEX = import.meta.env
+  .VITE_APP_TARGET_NETWORK_HEX as string;
+
 export const QUIZ_CONTRACT_ADDRESS = import.meta.env
   .VITE_APP_QUIZ_CONTRACT_ADDRESS as string;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,46 @@
+import { useWeb3 } from "react-dapp-web3";
 import { Grid } from "@mui/material";
 import QuizPresentation from "./components/QuizPresentation";
 import StartQuiz from "./components/StartQuiz";
 import Quiz from "./components/Quiz";
 import useQuiz from "../../hooks/app/useQuiz";
 import EQuizStatus from "../../constants/enums";
+import useNetwork from "../../hooks/app/useNetwork";
 
 const Home = () => {
+  const { isWalletConnected } = useWeb3();
   const { isLoading, quizData, quizStatus, handleQuizStatus } = useQuiz();
+  const { isTargetNetworkSelected } = useNetwork();
+
+  if (!isWalletConnected) {
+    return (
+      <main>
+        <Grid
+          sx={{ pt: 10, gap: 5 }}
+          container
+          flexDirection="column"
+          alignItems="center"
+        >
+          <h1>Login to begin</h1>
+        </Grid>
+      </main>
+    );
+  }
+
+  if (!isTargetNetworkSelected) {
+    return (
+      <main>
+        <Grid
+          sx={{ pt: 10, gap: 5 }}
+          container
+          flexDirection="column"
+          alignItems="center"
+        >
+          <h1>Change network to begin</h1>
+        </Grid>
+      </main>
+    );
+  }
 
   return (
     <main>
